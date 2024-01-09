155c1d20e0a44d8f17e3101a77f28b7877638174
Gouthami Yerramsetty <gouthsetty@gmail.com>
2024-01-09 19:45:55 +0100

    first commit

--- main.js
+++ main.js
@@ -28,7 +28,39 @@ You can choose what the actual content is.
 Try to use both the let and const keywords
 ******************************************************************************/
 
-//your code here
+// String
+let firstName = "Greta";
+let lastName = "Nora";
+
+let fullName = firstName + " " + lastName;
+
+const color = "Blue";
+
+// Number
+const price = 20;
+let age = 30;
+
+// Boolean
+let buttonClicked = false;
+const isValid = true;
+
+// Array
+let hobbies = ["Reading", "Swimming", "Cooking"];
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// Displaying the values
+console.log("String:", fullName, color);
+console.log("Number:", age, price);
+console.log("Boolean:", buttonClicked, isValid);
+console.log("Array:", hobbies, daysOfWeek);
 
 /****************************************************************************** 
 ASSIGNMENT 3
@@ -37,7 +69,44 @@ Try out a few of the operators we looked at (+, -, /, *)
 as well as a few of the shorthand operators (++, --, +=, -=)
 ******************************************************************************/
 
-//your code here
+// Basic arithmetic operations
+let a = 10;
+let b = 5;
+
+// Addition
+let sum = a + b;
+console.log("Sum:", sum); // Output: 15
+
+// Subtraction
+let difference = a - b;
+console.log("Difference:", difference); // Output: 5
+
+// Multiplication
+let product = a * b;
+console.log("Product:", product); // Output: 50
+
+// Division
+let division = a / b;
+console.log("Result:", division); // Output: 2
+
+// Shorthand operators
+let num = 2;
+
+// Increment (++)
+num++;
+console.log("Incremented:", num); 
+
+// Decrement (--)
+num--;
+console.log("Decremented:", num); 
+
+// Addition assignment (+=)
+num += 4;
+console.log("result:", num); 
+
+// Subtraction assignment (-=)
+num -= 3;
+console.log("result:", num); 
 
 /****************************************************************************** 
 ASSIGNMENT 4
@@ -63,7 +132,17 @@ let userIsLoggedIn = false;
 let userIsBlocked = false;
 let goToPage = "";
 
-//your code here
+// IF/ELSE conditional statement
+if (userName !== "" && userAge >= 18 && !userIsBlocked) {
+  // All conditions are true
+  userIsLoggedIn = true;
+  goToPage = "/home";
+  console.log(`Welcome, ${userName}! You are now logged in.`);
+} else {
+  console.log("Error: Unable to log in");
+}
+console.log("User Is Logged In:", userIsLoggedIn);
+
 
 /******************************************************************************
 ASSIGNMENT 5
@@ -78,6 +157,9 @@ Try changing userMale to both true and false and console.log your new variable,
 to see that your conditional is working.
 ******************************************************************************/
 
-const userMale = false;
+const userMale = true;
 
 //your code here
+const userTitle = userMale ? "Mr" : "Mrs";
+
+console.log(userTitle);
